aa994548fc48b9ba558c5f92a9417f23c3aeffd3
cjs675 <0x4a3b@proton.me>
2026-06-09 11:41:03 -0400

    move ch1 examples folder to ch1 dir

---
+++ ch1/examples/implDefBehavior/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+
+int main() 
+{
+    std::cout << sizeof(int) << '\n'; // print how many bytes of memory an int value takes 
+    std::cout << sizeof(double) << '\n'; // print how many bytes of memory a double value takes
+    
+    return 0;
+
+}
