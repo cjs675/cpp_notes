f53c510cb72180cc8c6e6073656c56c8a6b2dbbc
cjs675 <0x4a3b@proton.me>
2026-07-09 18:05:26 -0400

    add example working with bitwise left shift op

---
+++ scratch/bitwiseLeftShift/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+
+    cout << "Original Value: " << 5 << endl;   
+    cout << "Amount to be shifted: " << 2 << endl;
+    cout << "Value after bitwise shift left of 2 places: " << (5 << 2) << endl;
+    return 0;
+}
