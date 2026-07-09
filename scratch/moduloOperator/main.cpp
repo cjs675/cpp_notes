d6eca236872a61d33cd9efd0cacdbeb566eef82d
cjs675 <0x4a3b@proton.me>
2026-07-09 18:07:05 -0400

    add example working with modulo operator

---
+++ scratch/moduloOperator/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+
+    int a = 10, b = 3;
+
+    int result = a % b;
+
+    cout << "\nResult of 10 % 3 = " << result << endl;
+
+    return 0;
+}
