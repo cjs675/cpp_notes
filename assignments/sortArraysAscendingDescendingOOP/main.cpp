50dd4c1f85e6296088696c99953bf1946f0109f2
cjs675 <0x4a3b@proton.me>
2026-07-08 22:03:30 -0400

    feat: add method to sort array in ascending order

--- assignments/sortArraysAscendingDescendingOOP/main.cpp
+++ assignments/sortArraysAscendingDescendingOOP/main.cpp
@@ -25,12 +25,27 @@ public:
         cout << endl;
     }
 
-    void sortAscending() {
-
+    void sortAscending() {  
+        for (int i = 0; i < 9; i++) {  
+            for (int j = 0; j < 9 - i; j++) {
+                if (scores[j] > scores[j + 1]) {
+                    // swap values if score at index i > score at following index, [i + 1] 
+                    // temp = temporary var holding value to be swapped "out" of original place 
+                    int temp = scores[j + 1];
+                    scores[j + 1] = scores[j];
+                    scores[j] = temp;
+                }
+            }
+        } 
+        cout << "\nArray Sorted in Ascending Order: \n"; 
+        for (int i = 0; i < 10; i++) {
+            cout <<scores[i] << " ";
+        } 
+        cout << endl;
     }
 
     void sortDescending() {
-
+        int temp;
     }
 
     void locateHigh() {
@@ -45,7 +60,9 @@ public:
 int main() { 
 
     StudentScores scoreList1; 
+    
     scoreList1.getData();
-    scoreList1.displayOriginal();
+    scoreList1.displayOriginal(); 
+    scoreList1.sortAscending();
 
 }
