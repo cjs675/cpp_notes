ef0694e51617a19dc44ab000daaceba14886b940
cjs675 <0x4a3b@proton.me>
2026-07-17 07:30:46 -0400

    add program to calculate largest number in array

--- class_examples/arrays/largestNumInArray/main.cpp
+++ class_examples/arrays/largestNumInArray/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    int arr[5] = {12, 45, 23, 51, 19};
+    int  arr[5] = {12, 45, 23, 51, 19}; 
     int max = arr[0];
 
     for (int i = 1; i < 5; i++) {
@@ -11,8 +11,13 @@ int main() {
             max = arr[i];
         }
     }
-    cout << "Original Array: " << arr;
-    cout << "Max Element = " << max;
+    cout << "Original Array: ";
+    for (int i = 0; i < 5; i++) {
+        // print each element with a space 
+        cout << arr[i] << " ";
+    }
+
+    cout << "\nMax Element = " << max << endl;
 
     return 0;
 }
