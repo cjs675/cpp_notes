da0f349f77c6bb6015be10c5a539136295c55d28
cjs675 <0x4a3b@proton.me>
2026-07-20 15:39:42 -0400

    docs: add comments explaining different pass-by ops

--- assignments/assignment5_pointers/bankDeposit/main.cpp
+++ assignments/assignment5_pointers/bankDeposit/main.cpp
@@ -5,17 +5,23 @@ class Deposit {
 
     public:     
         Deposit () {}  
-
+        
+        // pass by value 
+        // makes a local clone 
         void depositByValue(double b, double depositAm) { 
             b = b + depositAm;
             cout << "\n[Value] Inside function - Balance after deposit: $" << b << endl; 
         }
-
+        // pass by reference 
+        // modifies original value residing at respective 
+        // memory address 
         void depositByReference(double &b, double depositAm) {
             b = b + depositAm;
             cout << "\n[Reference] Inside function - Balance after deposit: $" << b << endl;
         }
-
+        // pass by pointer 
+        // modifies original value by accessing it via 
+        // its memory address 
         void depositByPointer(double *b, double depositAm) {
             *b = *b + depositAm;
             cout << "\n[Pointer] Inside function - Balance after deposit: $" << *b << endl;
@@ -43,5 +49,7 @@ int main() {
 
     acc_1.depositByPointer(&balance, depositAm);
     cout << "\n[Pointer] After function call - Actual Balance: $" << balance << endl;
+
+
     return 0;
 }
