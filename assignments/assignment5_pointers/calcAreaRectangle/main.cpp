6877cd241d9c62bd0e5d3d99b94c3316fb1b8c07
cjs675 <0x4a3b@proton.me>
2026-07-20 15:11:01 -0400

    feat: add rectangular area calculator with references & pointers

--- assignments/assignment5_pointers/calcAreaRectangle/main.cpp
+++ assignments/assignment5_pointers/calcAreaRectangle/main.cpp
@@ -8,29 +8,64 @@ class Rectangle{
         int width;
 
     public: 
-        Rectangle(int l, int w) : length(l), width(w) {}; 
-
+        Rectangle() {}; 
+    
+    // creates copies of the original values 
+    // leaving the original unmodified 
     void AreaValue(int l, int w) {
+        l = 10; 
+        w = 3;
         int area = l * w; 
 
-        cout << "Pass-by-value: "  << area << endl;
+        cout << "\nPass by Value: "  << area << endl;
     } 
-
-    int AreaReference() {
+    
+    // accesses original data members and 
+    // changes their state
+    void AreaReference(int &l, int &w) {
+        l = 10;
+        w = 3;
+        int area = l * w;
+        cout << "\nPass by Reference: " << area << endl;
 
     }
     
-    int AreaPointer() {
-
+    // accesses original data members via 
+    // their memory address and changes their state  
+    // by dereference 
+    void AreaPointer(int *l, int *w) { 
+        *l = 10; 
+        *w = 3;
+        int area = (*l) * (*w);
+        cout << "\nPass by Pointer: " << area << endl;
     }
 
 };
 
+int main() { 
+    Rectangle r1;
+    int a, b;
 
+    cout << "\nEnter two values: " << endl;
+    cin >> a;
+    cin >> b; 
 
+    cout << "In main: " << a * b << endl;
 
+    // pass by value 
+    r1.AreaValue(a, b); 
+    // a & b --> unchanged since AreaValue only modifies copies 
+    cout << "\nIn main: " << a * b << endl;
 
-int main() {
+    // pass by reference 
+    r1.AreaReference(a, b);
+    // a & b --> changed to 10 & 3
+    cout << "\nIn main: " << a * b << endl;
+
+    // pass by pointer 
+    r1.AreaPointer(&a, &b);
+    // a & b --> stay as 10 & 3
+    cout << "\nIn main: " << a * b << endl;
     
     return 0;
 }
