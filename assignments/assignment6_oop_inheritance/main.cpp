148a1b765c6ed435e675b7adec176acdbc5447b7
cjs675 <0x4a3b@proton.me>
2026-07-21 10:50:06 -0400

    add initial scaffolding for assignment 6

---
+++ assignments/assignment6_oop_inheritance/main.cpp
@@ -0,0 +1,19 @@
+/* TODO: 
+* developer c++ program that illustrates hybrid inheritance 
+* which integrates 2+ inheritance types (single, multiple, multilevel
+* or hierarchical) w/in real-world application of your choice 
+
+* each class must contain >= 2 functional methods excluding display 
+*
+*/
+
+
+#include <iostream>
+
+
+
+
+int main() {
+    
+    return 0;
+}
