f1977824db836ce04cb8dbe5f8aa107d9a181cc6
cjs675 <0x4a3b@proton.me>
2026-07-26 13:49:35 -0400

    refactor: group methods calling area together

--- assignments/assignment7_compile_runtime_polymorphism/area_vol_rectangle/main.cpp
+++ assignments/assignment7_compile_runtime_polymorphism/area_vol_rectangle/main.cpp
@@ -91,10 +91,20 @@ public:
 int main() {
     Rectangle r1;
 
-    cout << r1.calculateArea(10, 13) << endl;
-    cout << r1.calculateVolume(5, 9, 8) << endl;
-    cout << r1.calculateArea(15.2, 9.1) << endl;
-    cout << r1.calculateVolume(8.19, 12.34, 29.902) << endl;
-    
+    cout << "\nArea of Rectangle using Integers: " <<
+         r1.calculateArea(10, 13) << endl;
+    // Append input float values with 'f' to force compiler
+    // to pass in floats
+    cout << "Area of Rectangle using Floats: " <<
+        r1.calculateArea(15.2f, 9.1f) << endl;
+    cout << "Area of Rectangle using Doubles: " <<
+        r1.calculateArea(10.284374, 5.9283741) << endl;
+
+    cout << "Volume of Rectangle using Integers: " <<
+        r1.calculateVolume(5, 9, 8) << endl;
+    cout << "Volume of Rectangle using Floats: " <<
+        r1.calculateVolume(8.19f, 12.34f, 29.902f) << endl;
+    cout << "Volume of Rectangle using Doubles: " <<
+        r1.calculateVolume(34.928324, 17.3849021, 29.01384) << endl;
     return 0;
 }
