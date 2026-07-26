b5a16eacd8158dd2dbd7491d4c3921ea4628ef44
cjs675 <0x4a3b@proton.me>
2026-07-26 15:06:57 -0400

    feat: add working bank class with overloaded methods for deposits

--- assignments/assignment7_compile_runtime_polymorphism/bankClass/main.cpp
+++ assignments/assignment7_compile_runtime_polymorphism/bankClass/main.cpp
@@ -3,30 +3,53 @@
 using namespace std;
 
 class Bank {
+// Balance initiated to zero to guard against garbage values
 private:
     double balance = 0.0;
 public:
+    // Method to deposit cash into account
     void deposit(double amount) {
-        balance += amount;
-        cout << "Deposited: $" << amount << endl;
+        if (amount > 0) {
+            balance += amount;
+            cout << "\nDeposited: $" << amount
+                 << " In Cash" << endl;
+            cout << "Account Balance: $" << balance << endl;
+        } else {
+           cout << "Error: Deposit Amount must be greater than $0.00" << endl;
+        }
     }
+    // Method to deposit cash and check into account
     void deposit(double amount, double checkAmount) {
-        balance += amount;
-        balance += checkAmount;
-        cout << "Deposited: $" << amount << "In Cash" <<
-                "Deposited: $" << checkAmount << "In Checks" << endl;
+        if (amount > 0 && checkAmount > 0) {
+            balance += amount;
+            balance += checkAmount;
+            cout << "\nDeposited: $" << amount << " In Cash"
+                 << "\nDeposited: $" << checkAmount << " In Checks" << endl;
+            cout << "Account Balance: $" << balance << endl;
+        } else {
+           cout << "Error: Deposit Amount must be greater than $0.00" << endl;
+        }
     }
+    // Method to perform online transfer of a given amount into account
     void deposit(string accountNum, double amount, string transferType) {
-        balance += amount;
-        cout << "Deposited: $" << amount << "In: " << accountNum <<
-                "Via: " << transferType << " has been completed" << endl;
+        if (!accountNum.empty() && amount > 0) {
+            balance += amount;
+            cout << "\nTransferred: $" << amount << " via: " << transferType
+                 << " to account: " << accountNum << endl;
+            cout << "Account Balance: $" << balance << endl;
+        } else {
+            cout << "Error: Invalid Account Number or Deposit Amount." << endl;
+        }
     }
 };
 
 int main() {
+    // Instantiation of Bank class object
     Bank account_1;
+
     account_1.deposit(1000);
-    account1.deposit(2000, 500);
+    account_1.deposit(2000, 500);
+    account_1.deposit("SAV1890131", 5000, "Online Transfer");
 
     return 0;
 }
